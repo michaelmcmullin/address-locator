282b5c2e895aef8d11729e6e9245fa0d7927da4b
mrmcnerd <blueark@outlook.com>
2017-06-28 07:29:47 +0100

    Adds a basic Location struct
    The Location struct contains latitude and longitude properties. It is immutable.

--- AddressLocator/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AddressLocator
-{
-    public class Class1
-    {
-    }
-}
---
+++ AddressLocator/Location.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AddressLocator
+{
+    /// <summary>
+    /// Latitude/Longitude co-ordinates of a location.
+    /// </summary>
+    public struct Location
+    {
+        private readonly double latitude;
+        private readonly double longitude;
+
+        /// <summary>
+        /// Constructor defining latitude and longitude.
+        /// </summary>
+        /// <param name="latitude">The latitude value to initialize this Location to.</param>
+        /// <param name="longitude">The longitude value to initialize this Location to.</param>
+        public Location(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        /// <summary>
+        /// Gets the Latitude value of this Location.
+        /// </summary>
+        public double Latitude { get { return latitude; } }
+
+        /// <summary>
+        /// Gets the Longitude value of this Location.
+        /// </summary>
+        public double Longitude { get { return longitude; } }
+
+        /// <summary>
+        /// A string representation of this Location.
+        /// </summary>
+        /// <returns>A string formatted as 'latitude, longitude'.</returns>
+        public override string ToString()
+        {
+            return $"{Latitude}, {Longitude}";
+        }
+    }
+}
