8cfd0d07c7f2da15534953b04cd3043f3f032c68
mrmcnerd <blueark@outlook.com>
2017-06-30 19:11:41 +0100

    Adds XML Documentation to DistanceKilometre class

--- AddressLocator/ConcreteClasses/DistanceKilometre.cs
+++ AddressLocator/ConcreteClasses/DistanceKilometre.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace AddressLocator
 {
+    /// <summary>
+    /// Stores distance values in kilometers
+    /// </summary>
     class DistanceKilometre : Distance
     {
+        /// <summary>
+        /// The amount to multiply this distance by to convert it to metres.
+        /// </summary>
         public override double ValueToMetresRatio => 1000;
 
+        /// <summary>
+        /// The name of this distance unit.
+        /// </summary>
         public override string Name => "Kilometre";
 
+        /// <summary>
+        /// The abbreviated name of this distance unit.
+        /// </summary>
         public override string Abbreviation => "km";
     }
 }
